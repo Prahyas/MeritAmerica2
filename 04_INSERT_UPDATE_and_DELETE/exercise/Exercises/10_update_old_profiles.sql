bfb8aa9f8bc0d521df2bb04ea621c3cd98174725
Prayash Shrestha <xrestha17@gmail.com>
2024-04-29 14:25:54 -0500

    completed insert update exercise

--- 04_INSERT_UPDATE_and_DELETE/exercise/Exercises/10_update_old_profiles.sql
+++ 04_INSERT_UPDATE_and_DELETE/exercise/Exercises/10_update_old_profiles.sql
@@ -2,3 +2,7 @@
 --     home_page to "No image" and their profile_path to NULL (64 rows)
 
 
+UPDATE person
+SET home_page = 'No image', profile_path = NULL
+WHERE EXTRACT(YEAR FROM birthday) < 1900 AND
+      (profile_path IS NULL OR RIGHT(profile_path, 4) != '.jpg');
