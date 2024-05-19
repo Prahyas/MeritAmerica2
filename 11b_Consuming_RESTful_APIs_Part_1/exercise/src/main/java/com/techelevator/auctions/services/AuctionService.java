39c71ed5ade6a554636e9afe6f01fdfd51f32c7b
Prayash Shrestha <xrestha17@gmail.com>
2024-05-19 13:54:46 -0500

    completed rest api part 1

--- 11b_Consuming_RESTful_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/services/AuctionService.java
+++ 11b_Consuming_RESTful_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/services/AuctionService.java
@@ -1,8 +1,9 @@
 package com.techelevator.auctions.services;
 
-import org.springframework.web.client.RestTemplate;
-
 import com.techelevator.auctions.model.Auction;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
 
 public class AuctionService {
 
@@ -11,23 +12,59 @@ public class AuctionService {
 
 
     public Auction[] getAllAuctions() {
-        // call api here
-        return null;
+        Auction[] auctions = null;
+        try {
+            auctions = restTemplate.getForObject(API_BASE_URL, Auction[].class);
+        } catch (RestClientResponseException e) {
+            // handles exceptions thrown by rest template and contains status codes
+            System.out.println(e.getRawStatusCode() + " : " + e.getStatusText());
+        } catch (ResourceAccessException e) {
+            // i/o error, ex: the server isn't running
+            System.out.println(e.getMessage());
+        }
+        return auctions;
     }
 
     public Auction getAuction(int id) {
-        // call api here
-        return null;
+        Auction auction = null;
+        try {
+            auction = restTemplate.getForObject(API_BASE_URL + "/" + id, Auction.class);
+        } catch (RestClientResponseException e) {
+            // handles exceptions thrown by rest template and contains status codes
+            System.out.println(e.getRawStatusCode() + " : " + e.getStatusText());
+        } catch (ResourceAccessException e) {
+            // i/o error, ex: the server isn't running
+            System.out.println(e.getMessage());
+        }
+        return auction;
     }
 
     public Auction[] getAuctionsMatchingTitle(String title) {
-        // call api here
-        return null;
+        Auction[] auctions = null;
+        try {
+            auctions = restTemplate.getForObject(API_BASE_URL + "?title_like=" + title, Auction[].class);
+        } catch (RestClientResponseException e) {
+            // handles exceptions thrown by rest template and contains status codes
+            System.out.println(e.getRawStatusCode() + " : " + e.getStatusText());
+        } catch (ResourceAccessException e) {
+            // i/o error, ex: the server isn't running
+            System.out.println(e.getMessage());
+        }
+        return auctions;
     }
 
     public Auction[] getAuctionsAtOrBelowPrice(double price) {
-        // call api here
-        return null;
+        Auction[] auctions = null;
+        try {
+            auctions = restTemplate.getForObject(API_BASE_URL + "?currentBid_lte=" + price, Auction[].class);
+        } catch (RestClientResponseException e) {
+            // handles exceptions thrown by rest template and contains status codes
+            System.out.println(e.getRawStatusCode() + " : " + e.getStatusText());
+        } catch (ResourceAccessException e) {
+            // i/o error, ex: the server isn't running
+            System.out.println(e.getMessage());
+        }
+        return auctions;
     }
 
 }
